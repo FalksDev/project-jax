5041402a02cfe8d47b349b60698f39b130c9d635
Daniel Falk <falks.dev@gmail.com>
2023-06-22 23:16:12 +0200

    set up vue with tailwindcss

---
+++ apps/web-client/src/components/WelcomeItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3 
+      class="
+      text-3xl 
+      font-medium
+      bg-primary
+      hover:bg-primary-hover
+      transition-all duration-200
+      rounded-md m-2 p-2">
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
--- apps/web-client/tailwind.config.js
+++ apps/web-client/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: "rgb(var(--color-primary))",
+        light: "rgb(var(--color-primary-light))",
+        dark: "rgb(var(--color-primary-dark))",
+        hover: "rgb(var(--color-primary) / 0.6)",
+        'dark-hover': "rgb(var(--color-primary-dark) / 0.6)",
+        'light-hover': "rgb(var(--color-primary-light) / 0.6)"
+      },
+      background: {
+        root: "#1e293b",
+        DEFAULT: "#475569"
+      },
+      normal: {
+        DEFAULT: "#e2e8f0"
+      }
+    },
   },
   plugins: [],
 }
